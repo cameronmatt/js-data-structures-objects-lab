66ea5c3126c61bcf8f9993cd170574d4685838b9
Matthew <cameron.matt@outlook.com>
2021-03-05 08:38:34 +1100

    data st. objects done

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+let driver = { 
+    name: 'Sam',
+    address: '11 Broadway'
+    };
+  
+  function updateDriverWithKeyAndValue(driver, address, value) {
+    return Object.assign({}, driver, { [address]: '11 Broadway' });
+  }
+
+  function destructivelyUpdateDriverWithKeyAndValue(employee, address, value){
+    employee[address] = '12 Broadway'
+    return employee
+  }
+
+  function deleteFromDriverByKey(driver, key){
+    let newDriver =  Object.assign({},driver);
+    delete newDriver.name;
+    return newDriver;
+  }
+
+  function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver.name;
+    return driver;
+  }
